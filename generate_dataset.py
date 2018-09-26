c1bfd6ee470f09fe9715a250eb910325a78d20fe
jbuisine <jerome.buisine@univ-littoral.fr>
2018-09-26 16:47:44 +0200

    Update of generate dataset and add of config file

--- generate_dataset.py
+++ generate_dataset.py
@@ -7,21 +7,30 @@ Created on Fri Sep 14 21:02:42 2018
 """
 
 from __future__ import print_function
-import os, glob, image_slicer
+import glob, image_slicer
+import sys, os, getopt
 from PIL import Image
+import shutil
 
 # show to create own dataset https://gist.github.com/fchollet/0830affa1f7f19fd47b06d4cf89ed44d
 
 NUMBER_SUB_IMAGES = 100
 
 def init_directory():
-    if not os.path.exists('data'):
 
+    if os.path.exists('data'):
+        print("Removing all previous data...")
+
+        shutil.rmtree('data')
+
+    if not os.path.exists('data'):
+        print("Creating new data...")
         os.makedirs('data/train/final')
         os.makedirs('data/train/noisy')
 
         os.makedirs('data/validation/final')
         os.makedirs('data/validation/noisy')
+       
 
 def create_images(folder, output_folder):
     images_path = glob.glob(folder + "/*.png")
@@ -38,6 +47,30 @@ def generate_dataset():
     create_images('img_validation/noisy', 'data/validation/noisy')
 
 def main():
+
+    global NUMBER_SUB_IMAGES
+
+    if len(sys.argv) <= 1:
+        print('Please specify nb sub image per image parameter (use -h if you want to know values)...')
+        print('generate_dataset.py --nb xxxx')
+        sys.exit(2)
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hn", ["help", "nb="])
+    except getopt.GetoptError:
+        # print help information and exit:
+        print('generate_dataset.py --nb xxxx')
+        sys.exit(2)
+    for o, a in opts:
+        if o == "-h":
+            print('generate_dataset.py --nb xxxx')
+            print('20x20 : 1600')
+            print('40x40 : 400')
+            print('60x60 : 178 (approximately)')
+            print('80x80 : 100')
+            print('100x100 : 64')
+            sys.exit()
+        elif o == '-n':
+            NUMBER_SUB_IMAGES = int(a)
 
     init_directory()
 
