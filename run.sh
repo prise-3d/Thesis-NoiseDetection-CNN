d115e958976beff7a0b82afaa2c690ea8a3b37cd
jbuisine <jerome.buisine@univ-littoral.fr>
2018-09-27 11:17:06 +0200

    Update of script order

--- run.sh
+++ run.sh
@@ -8,6 +8,6 @@ else
   echo "Need size parameter : ./run.sh 20"; 
 fi
 
-python classification_cnn_keras_svd.py --directory ../models/$size/ --output svd_model --batch_size 32 --epochs 150 --img $size
 python classification_cnn_keras.py --directory ../models/$size/ --output cnn_model --batch_size 32 --epochs 150 --img $size
 python classification_cnn_keras_cross_validation.py --directory ../models/$size/ --output cnn_cross_validation_model --batch_size 32 --epochs 150 --img $size
+python classification_cnn_keras_svd.py --directory ../models/$size/ --output svd_model --batch_size 32 --epochs 150 --img $size
