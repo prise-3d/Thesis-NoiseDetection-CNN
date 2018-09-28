7035cdd867f8fdee41c9fa9ad06cab27822dcf68
jbuisine <jerome.buisine@univ-littoral.fr>
2018-09-28 12:38:47 +0200

    Model CCN layers Updates

--- classification_cnn_keras_cross_validation.py
+++ classification_cnn_keras_cross_validation.py
@@ -70,14 +70,6 @@ def generate_model():
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
-    model.add(Conv2D(40, (2, 2)))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-
-    model.add(Conv2D(20, (2, 2)))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-
     model.add(Flatten())
 
     model.add(Dense(140))
