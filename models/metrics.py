b509851b5d690381131ae4570ac48a0a2f199ab1
Jérôme BUISINE <contact@jeromebuisine.fr>
2019-07-19 11:48:59 +0200

    Add of models folder with metrics

---
+++ models/metrics.py
@@ -0,0 +1,9 @@
+# model imports
+from keras import backend as K
+import tensorflow as tf
+
+def auc(y_true, y_pred):
+    auc = tf.metrics.auc(y_true, y_pred)[1]
+    K.get_session().run(tf.local_variables_initializer())
+    
+    return auc
