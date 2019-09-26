643faba63c0ec8261d01018d6aefae80b4a8def5
Jérôme BUISINE <contact@jeromebuisine.fr>
2019-09-26 17:43:05 +0200

    Update of symlinks script generator

--- generate_symlinks.sh
+++ generate_symlinks.sh
@@ -10,6 +10,14 @@ fi
 path=$1
 
 for link in {"data","results","saved_models","models_infos","models_backup","threshold_map","learned_zones","custom_norm"}; do
-    rm ${link}
-    ln -s $1/${link} ${link}
+    
+    if [ -L ${link} ]; then
+        rm ${link}
+    fi
+    
+    if [ ! -d "${path}/${link}" ]; then
+        mkdir -p ${path}/${link}
+    fi
+    
+    ln -s ${path}/${link} ${link}
 done
