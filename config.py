0193803d6e6f75b08a9a29cea20afb1bb208fb62
Jérôme BUISINE <contact@jeromebuisine.fr>
2021-03-24 09:10:13 +0100

    Update configuration params

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ results_filename                = 'results.csv'
 
 # variables
 perf_train_header_file          = "model_name;global_train_size;global_test_size;filtered_train_size;filtered_test_size;f1_train;f1_test;recall_train;recall_test;presicion_train;precision_test;acc_train;acc_test;roc_auc_train;roc_auc_test;\n"
-perf_prediction_header_file    = "data;data_size;model_name;accucary;f1;recall;precision;roc;\n"
+perf_prediction_header_file     = "data;data_size;model_name;accucary;f1;recall;precision;roc;\n"
 
 features_choices_labels         = ['static', 'svd_reconstruction', 'svd_reconstruction_dyn', 'fast_ica_reconstruction', 'ipca_reconstruction', 'min_diff_filter', 'sobel_based_filter','nl_mean_noise_mask', 'gini_map']
 
@@ -38,5 +38,6 @@ keras_epochs                    = 30
 keras_img_size                  = (200, 200)
 
 # parameters
-scene_image_quality_separator     = '_'
-scene_image_extension             = '.png'
+post_image_name_separator       = '___'
+scene_image_quality_separator   = '_'
+scene_image_extension           = '.png'
